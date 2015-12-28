bc83b277d0c55beb0ca439ab5b8f0639c8b27961
Damian Farina <damianfarina@gmail.com>
2015-12-28 00:33:42 -0300

    Sample task

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
-u = User.new(username: 'damianfarina')
-u.password = 'password'
-u.save
+# Users
+u = User.create(username: 'damianfarina', password: 'password')
+
+# Tasks
+u.add_task(Task.new({
+  description: 'Description',
+  category: 'personal',
+  due_date: Date.today
+}))
