237211ee2617396920f0e05615fc1bbf77680a3b
Damian Farina <damianfarina@gmail.com>
2015-12-28 22:59:44 -0300

    More sample users and tasks read/unread

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,37 @@
 # Users
-u = User.create(username: 'damianfarina', password: 'password')
+u = User.create(username: 'test1', password: 'password')
 
 # Tasks
-u.add_task(Task.new({
-  description: 'Description',
-  category: 'personal',
-  due_date: Date.today
-}))
+(1..10).step(2) do |i|
+  t = Task.new({
+    description: "Description 1-#{i}",
+    category: 'personal',
+    due_date: Date.today
+  })
+  u.add_task t
+  u.add_read_task t
+  u.add_task({
+    description: "Description 1-#{i + 1}",
+    category: 'school',
+    due_date: Date.today
+  })
+end
+
+# Users
+u = User.create(username: 'test2', password: 'password')
+
+# Tasks
+(1..10).step(2) do |i|
+  t = Task.new({
+    description: "Description 2-#{i}",
+    category: 'personal',
+    due_date: Date.today
+  })
+  u.add_task t
+  u.add_read_task t
+  u.add_task({
+    description: "Description 2-#{i + 1}",
+    category: 'school',
+    due_date: Date.today
+  })
+end
